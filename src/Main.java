027d5e85871416b7c156b7caafb0f292316f0910
iON1k <iON1k88@gmail.com>
2019-07-06 22:20:54 +0300

    Задание 2

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,43 @@ import java.util.Scanner;
 public class Main {
     public static void main (String args[]){
         Scanner sc = new Scanner(System.in);
-        System.out.println("кот, собака или мышь?");
-        String string = sc.nextLine();
+        String string = null;
+        JFrame frame = null;
 
-        if (string.equals("кот")) {
-            JFrame frame = new JFrame("FirstForm");
-            frame.setContentPane(new FirstForm().panel);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.pack();
-            frame.setVisible(true);
-        } else if (string.equals("собака")) {
-            JFrame frame = new JFrame("SecondForm");
-            frame.setContentPane(new SecondForm().panel);
-            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-            frame.pack();
-            frame.setVisible(true);
-        }
+        do {
+            System.out.println("кот или собака?");
+            string = sc.nextLine();
+
+            if (string.equals("кот")) {
+                if (frame != null) {
+                    frame.setVisible(false);
+                    frame.dispose();
+                }
+                frame = new JFrame("FirstForm");
+                frame.setContentPane(new FirstForm().panel);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setLocationRelativeTo(null);
+                frame.pack();
+                frame.setVisible(true);
+            } else if (string.equals("собака")) {
+                if (frame != null) {
+                    frame.setVisible(false);
+                    frame.dispose();
+                }
+                frame = new JFrame("SecondForm");
+                frame.setContentPane(new SecondForm().panel);
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setLocationRelativeTo(null);
+                frame.pack();
+                frame.setVisible(true);
+            } else if (string.isEmpty()) {
+                if (frame != null) {
+                    frame.setVisible(false);
+                    frame.dispose();
+                }
+            } else {
+                System.out.println("Вы ввели неправильное животное.");
+            }
+        } while (!string.isEmpty());
     }
 }
